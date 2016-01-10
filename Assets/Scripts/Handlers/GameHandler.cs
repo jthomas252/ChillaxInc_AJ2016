d4560fe6c6742aaff42b84fee6e4ffcd674b2654
Jonathan Thomas <jthom252@gmail.com>
2016-01-10 07:35:37 -0800

    Pre-Sunday Changes

--- Assets/Scripts/Handlers/GameHandler.cs
+++ Assets/Scripts/Handlers/GameHandler.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Game : MonoBehaviour {
+namespace Relax.Handlers {
+    public class Game : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
 }
